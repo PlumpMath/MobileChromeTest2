f260435539f026066f4b6057a1c41298a4e25eb0
Adam Singer <financeCoding@gmail.com>
2013-12-10 18:44:40 -0800

    adding files

---
+++ .cordova/hooks/after_prepare/mca-update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd "/Users/adam.singer/dart/dart-mobile-chrome-app/PolymerDartDemo2"
+"node" "/Users/adam.singer/dart/mobile-chrome-apps/mca" update-app
---
+++ platforms/android/assets/www/plugins/org.chromium.bootstrap/api/helpers/ChromeExtensionURLs.android.js
@@ -0,0 +1,11 @@
+cordova.define("org.chromium.bootstrap.helpers.ChromeExtensionURLs", function(require, exports, module) {// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+exports.releaseReadyWait = function() {
+  Object.defineProperty(document, 'readyState', {get: function() { return 'complete'}, configurable: true });
+  cordova.fireDocumentEvent('DOMContentLoaded');
+  cordova.fireWindowEvent('DOMContentLoaded');
+  cordova.fireWindowEvent('load');
+};
+});
